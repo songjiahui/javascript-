d868689171837e03815ba0f56d866e1522f8c38a
songjiahui <36881668+songjiahui@users.noreply.github.com>
2018-06-19 20:36:57 +0800

    新春贺卡
    使用HTML5、CSS3开发出移动端电子贺卡，该贺卡有背景音乐，操作者可以通过手机触摸控制停止或开始，贺卡自动进行图片转换，共有三张背景图。

---
+++ 新春贺卡/js/index.js
@@ -0,0 +1,36 @@
+window.onload=function(){
+	var page1=document.getElementById("page1");
+	var page2=document.getElementById("page2");
+	var page3=document.getElementById("page3");
+	var music=document.getElementById("music");
+var audio=document.getElementsByTagName("audio")[0];
+//音乐停止，自动停止光盘旋转效果
+audio.addEventListener("ended",function(event){
+	music.setAttribute("class","");
+	
+},false)
+  music.addEventListener("touchstart",function (event) {
+    		if(audio.paused){
+		audio.play();
+		this.setAttribute("class","play");
+//		this.style.animationPlayState="running";
+
+	}
+	else{
+		audio.pause();
+		this.setAttribute("class","");
+//      this.style.animationPlayState="paused";
+	}
+  },false);
+  page1.addEventListener("touchstart",function (event) {
+  	page1.style.display="none";
+  	page2.style.display="block";
+  	page3.style.display="block";
+  	page3.style.top="100%";
+  	setTimeout(function () {
+  		page2.setAttribute("class","page fadeOut");
+  		page3.setAttribute("class","page fadeIn");
+  	},5500);
+  },false);
+ 
+}
